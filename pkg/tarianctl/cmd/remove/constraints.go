8008c56d32f56a0b33848a1c3af564021a6fc61d
Andy Librian <andylibrian@gmail.com>
2021-11-20 13:54:51 +0700

    feat(tarianctl): update remove constraints to accept multiple names

--- pkg/tarianctl/cmd/remove/constraints.go
+++ pkg/tarianctl/cmd/remove/constraints.go
@@ -14,8 +14,10 @@ import (
 
 func NewRemoveConstraintsCommand() *cli.Command {
 	return &cli.Command{
-		Name:  "constraints",
-		Usage: "Remove constraints from the Tarian Server.",
+		Name:      "constraints",
+		Aliases:   []string{"constraint"},
+		Usage:     "Remove constraints from the Tarian Server.",
+		UsageText: "Tarianctl remove constraints [command options] names...",
 		Flags: []cli.Flag{
 			&cli.StringFlag{
 				Name:    "namespace",
@@ -23,30 +25,30 @@ func NewRemoveConstraintsCommand() *cli.Command {
 				Usage:   "The namespace scope for the constraint to be removed",
 				Value:   "default",
 			},
-			&cli.StringFlag{
-				Name:     "name",
-				Usage:    "The name scope for the constraint to be removed",
-				Value:    "",
-				Required: true,
-			},
 		},
 		Action: func(c *cli.Context) error {
+			if c.Args().Len() == 0 {
+				cli.ShowSubcommandHelpAndExit(c, 1)
+			}
+
 			logger := logger.GetLogger(c.String("log-level"), c.String("log-encoding"))
 			util.SetLogger(logger)
 
 			opts := util.ClientOptionsFromCliContext(c)
 			client, _ := client.NewConfigClient(c.String("server-address"), opts...)
 
-			ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-			response, err := client.RemoveConstraint(ctx, &tarianpb.RemoveConstraintRequest{Namespace: c.String("namespace"), Name: c.String("name")})
-			cancel()
+			for _, name := range c.Args().Slice() {
+				ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+				response, err := client.RemoveConstraint(ctx, &tarianpb.RemoveConstraintRequest{Namespace: c.String("namespace"), Name: name})
+				cancel()
 
-			if err != nil {
-				logger.Fatal(err)
-			}
+				if err != nil {
+					logger.Fatal(err)
+				}
 
-			if response.GetSuccess() {
-				fmt.Println("Constraint is deleted succesfully")
+				if response.GetSuccess() {
+					fmt.Printf("Constraint %s is deleted succesfully\n", name)
+				}
 			}
 
 			return nil
