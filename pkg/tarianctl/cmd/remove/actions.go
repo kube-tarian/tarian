e48530f8556bfe1a1f81e2c19b99251ba649b58d
Andy Librian <andylibrian@gmail.com>
2021-12-03 05:41:36 +0700

    feat(tarianctl): update remove actions to accept multiple names

--- pkg/tarianctl/cmd/remove/actions.go
+++ pkg/tarianctl/cmd/remove/actions.go
@@ -14,9 +14,10 @@ import (
 
 func NewRemoveActionsCommand() *cli.Command {
 	return &cli.Command{
-		Name:    "actions",
-		Usage:   "Remove actions from the Tarian Server.",
-		Aliases: []string{"action"},
+		Name:      "actions",
+		Usage:     "Remove actions from the Tarian Server.",
+		UsageText: "Tarianctl remove actions [command options] names...",
+		Aliases:   []string{"action"},
 		Flags: []cli.Flag{
 			&cli.StringFlag{
 				Name:    "namespace",
@@ -24,30 +25,30 @@ func NewRemoveActionsCommand() *cli.Command {
 				Usage:   "The namespace scope for the action to be removed",
 				Value:   "default",
 			},
-			&cli.StringFlag{
-				Name:     "name",
-				Usage:    "The name of the action to be removed",
-				Value:    "",
-				Required: true,
-			},
 		},
 		Action: func(c *cli.Context) error {
+			if c.Args().Len() == 0 {
+				cli.ShowSubcommandHelpAndExit(c, 1)
+			}
+
 			logger := logger.GetLogger(c.String("log-level"), c.String("log-encoding"))
 			util.SetLogger(logger)
 
 			opts := util.ClientOptionsFromCliContext(c)
 			client, _ := client.NewConfigClient(c.String("server-address"), opts...)
 
-			ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-			response, err := client.RemoveAction(ctx, &tarianpb.RemoveActionRequest{Namespace: c.String("namespace"), Name: c.String("name")})
-			cancel()
+			for _, name := range c.Args().Slice() {
+				ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+				response, err := client.RemoveAction(ctx, &tarianpb.RemoveActionRequest{Namespace: c.String("namespace"), Name: name})
+				cancel()
 
-			if err != nil {
-				logger.Fatal(err)
-			}
+				if err != nil {
+					logger.Fatal(err)
+				}
 
-			if response.GetSuccess() {
-				fmt.Println("Action is deleted succesfully")
+				if response.GetSuccess() {
+					fmt.Printf("Action %s is deleted succesfully\n", name)
+				}
 			}
 
 			return nil
